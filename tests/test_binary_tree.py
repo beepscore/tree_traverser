ecc9c60ee4d6b2f89f8b719e94659efb47a18d21
beepscore <support@beepscore.com>
2017-05-02 15:52:46 -0700

    Add test_is_valid_binary_search_tree_four_levels

--- tests/test_binary_tree.py
+++ tests/test_binary_tree.py
@@ -97,104 +97,44 @@ class TestTree(unittest.TestCase):
 
         self.assertTrue(binary_tree.is_valid_binary_search_tree(node0, {}))
 
-    # def test_get_node_with_value_three_levels(self):
-    #     node0 = node.Node(3)
-    #     node1 = node.Node(5)
-    #     node2 = node.Node(2)
-    #     node3 = node.Node(2)
-    #     node4 = node.Node(3)
-    #     node5 = node.Node(1)
-    #
-    #     node0.children = [node1, node2]
-    #     node1.children = [node3, node4]
-    #     node2.children = [node5]
-    #
-    #     actual = tree.get_node_with_value(node0, 1)
-    #     self.assertEqual(actual, node5)
-    #
-    # def test_get_node_with_value_three_levels_not_leaf(self):
-    #     """
-    #              3
-    #            /  \
-    #           /    \
-    #          /      \
-    #         5       2
-    #        / \     /
-    #       2   3   1
-    #     """
-    #     node0 = node.Node(3)
-    #     node1 = node.Node(5)
-    #     node2 = node.Node(2)
-    #     node3 = node.Node(2)
-    #     node4 = node.Node(3)
-    #     node5 = node.Node(1)
-    #
-    #     node0.children = [node1, node2]
-    #     node1.children = [node3, node4]
-    #     node2.children = [node5]
-    #
-    #     actual = tree.get_node_with_value(node0, 5)
-    #     self.assertEqual(actual, node1)
-    #
-    # def test_get_node_with_value_three_levels_root(self):
-    #     """
-    #              7
-    #            /  \
-    #           /    \
-    #          /      \
-    #         5       2
-    #        / \     /
-    #       6   3   1
-    #     """
-    #     node0 = node.Node(7)
-    #     node1 = node.Node(5)
-    #     node2 = node.Node(2)
-    #     node3 = node.Node(6)
-    #     node4 = node.Node(3)
-    #     node5 = node.Node(1)
-    #
-    #     node0.children = [node1, node2]
-    #     node1.children = [node3, node4]
-    #     node2.children = [node5]
-    #
-    #     actual = tree.get_node_with_value(node0, 7)
-    #     self.assertEqual(actual, node0)
-    #
-    # def test_get_node_with_value_non_binary_four_levels(self):
-    #     """
-    #     non-binary tree
-    #
-    #              7
-    #            /  \
-    #           /    \
-    #          /      \
-    #         5       2
-    #        / \     /
-    #       6   3   1
-    #          /| \
-    #         / |  \
-    #        8  9   1
-    #     """
-    #     node0 = node.Node(7)
-    #     node1 = node.Node(5)
-    #     node2 = node.Node(2)
-    #     node3 = node.Node(6)
-    #     node4 = node.Node(3)
-    #     node5 = node.Node(1)
-    #     node6 = node.Node(8)
-    #     node7 = node.Node(9)
-    #     node8 = node.Node(1)
-    #
-    #     node0.children = [node1, node2]
-    #     node1.children = [node3, node4]
-    #     node2.children = [node5]
-    #     node4.children = [node6, node7, node8]
-    #
-    #     self.assertEqual(tree.get_node_with_value(node0, 7), node0)
-    #     self.assertEqual(tree.get_node_with_value(node0, 5), node1)
-    #     self.assertEqual(tree.get_node_with_value(node0, 2), node2)
-    #     self.assertEqual(tree.get_node_with_value(node0, 6), node3)
-    #     self.assertEqual(tree.get_node_with_value(node0, 3), node4)
-    #     self.assertEqual(tree.get_node_with_value(node0, 3), node4)
-    #     self.assertEqual(tree.get_node_with_value(node0, 9), node7)
-    #     self.assertEqual(tree.get_node_with_value(node0, 1), node8)
+    def test_is_valid_binary_search_tree_four_levels(self):
+        """
+                 5
+               /  \
+              /    \
+             /      \
+            4        7
+           / \      /
+          2   9    6
+             / \
+            8   11
+        """
+        node0 = binary_node.BinaryNode(5)
+        node1 = binary_node.BinaryNode(4)
+        node2 = binary_node.BinaryNode(7)
+        node3 = binary_node.BinaryNode(2)
+        node4 = binary_node.BinaryNode(9)
+        node5 = binary_node.BinaryNode(6)
+        node6 = binary_node.BinaryNode(8)
+        node7 = binary_node.BinaryNode(1)
+
+        node0.left = node1
+        node0.right = node2
+
+        node1.left = node3
+        node1.right = node4
+
+        node2.left = node5
+
+        node4.left = node6
+        node4.right = node7
+
+        self.assertFalse(binary_tree.is_valid_binary_search_tree(node0, {}))
+
+        # manually check subtrees
+        self.assertFalse(binary_tree.is_valid_binary_search_tree(node1, {}))
+        self.assertTrue(binary_tree.is_valid_binary_search_tree(node2, {}))
+
+        self.assertTrue(binary_tree.is_valid_binary_search_tree(node3, {}))
+        self.assertFalse(binary_tree.is_valid_binary_search_tree(node4, {}))
+
